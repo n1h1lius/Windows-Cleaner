d53ed555e162eabb087b64b870923fb2a5567ae0
Nihilus Null Zero <135299351+n1h1lius@users.noreply.github.com>
2026-01-12 22:28:07 +0100

    Legacy Update 2

--- Versions/Cleaner_v1/cleaner_v1.py
+++ Versions/Cleaner_v1/cleaner_v1.py
@@ -4,7 +4,7 @@ import time
 import msvcrt
 
 from Scripts.core.Cleaner import detect_and_get_paths
-from Scripts.config import DAYS_THRESHOLD, detected_folders
+from Scripts.config import DAYS_THRESHOLD
 from colorama import Fore, Style, init
 
 init(autoreset=True)
@@ -20,31 +20,7 @@ numVars = {
 
 VERBOSE_MODE = False
 
-USER_PROFILE = os.environ.get("USERPROFILE")
-APPDATA_LOCAL = "\\AppData\\Local"
-APPDATA_LOCALLOW = "\\AppData\\LocalLow"
-APPDATA_ROAMING = "\\AppData\\Roaming"
-
-EDGE_PATH = USER_PROFILE + APPDATA_LOCAL + "\\Microsoft\\Edge"
-BRAVE_PATH = USER_PROFILE + APPDATA_LOCAL + "\\BraveSoftware\\Brave-Browser"
-CHROME_PATH = USER_PROFILE + APPDATA_LOCAL + "\\Google\\Chrome"
-
-DISCORD_PATH = USER_PROFILE +  APPDATA_ROAMING + "\\discord"
-SPOTIFY_PATH = USER_PROFILE +  APPDATA_LOCAL + "\\Spotify"
-
-BROWSERS_PATHS = [
-        "\\Cache",
-        "\\File System",
-        "\\IndexedDB",
-        "\\Code Cache",
-        "\\Service Worker",
-        "\\GPU_Cache",
-        "\\blob_storage"
-    ]
-
-# Configuración avanzada
-
-SIZE_THRESHOLD_MB = None  # Tamaño máximo permitido en MB para archivos None for none
+SIZE_THRESHOLD_MB = None  # Max size threshold in MB
 
 # =================================================================================================================
 #                                                   UTILITIES
@@ -60,13 +36,13 @@ def wait_before_continue(timeout=10):
             return
 
 def is_file_old(file_path, days_threshold):
-    """Verifica si un archivo no se ha modificado en un tiempo específico."""
+    """Verifies if a file has not been modified in a specific time."""
     file_age = time.time() - os.path.getmtime(file_path)
     return file_age > days_threshold * 86400  # Convertir días a segundos
 
 
 def is_file_large(file_path, size_threshold_mb):
-    """Verifica si un archivo supera el tamaño especificado en MB."""
+    """Verifies if a file exceeds the specified size in MB."""
     file_size_mb = os.path.getsize(file_path) / (1024 * 1024)  # Convertir bytes a MB
     return file_size_mb > size_threshold_mb
 
@@ -74,124 +50,12 @@ def is_file_large(file_path, size_threshold_mb):
 def get_file_size(file_path):
     return round(os.path.getsize(file_path) / (1024 * 1024), 2)  # Convertir bytes a MB
 
-def get_browser_profiles(userData_path):
-    paths = []
-    counter = 0
-
-    # Profile Folders
-    for dir in os.listdir(userData_path):
-        if dir.startswith("Profile"):
-            profile_path = userData_path + f"\\{dir}"
-
-            for path in BROWSERS_PATHS:
-                paths.append(profile_path + path)
-
-            counter += 1
-
-    return [paths, counter]
-
-def get_paths_to_clean():
-    
-    paths = [
-        "C:\\Windows\\Temp",
-        "C:\\Windows\\Prefetch",
-        "C:\\$Recycle.Bin",
-        USER_PROFILE + APPDATA_LOCAL + "\\Temp",
-        USER_PROFILE + APPDATA_LOCALLOW + "\\Temp",
-        USER_PROFILE + APPDATA_LOCAL + "\\CrashDumps",
-        USER_PROFILE + APPDATA_LOCAL + "\\D3DSCache",
-        USER_PROFILE + APPDATA_LOCAL + "\\Microsoft\\Windows\\Explorer"
-    ]
-
-    print(Fore.CYAN + f"\n///// -> DETECTING INSTALLED PROGRAMS\n")
-    
-    # Microsoft Edge
-    # ---------------------------------------------------------------------
-
-    if os.path.isdir(EDGE_PATH):
-        default_path = EDGE_PATH + "\\User Data\\Default"
-        
-        paths.append(default_path)
-
-        print(Fore.LIGHTGREEN_EX + "///// -> Microsoft Edge Detected")
-    
-    # Brave
-    # ---------------------------------------------------------------------
-
-    if os.path.isdir(BRAVE_PATH):
-        userData_path = BRAVE_PATH + "\\User Data"
-        default_path = userData_path + "\\Default"
-        
-        # Basic Folder
-        for path in BROWSERS_PATHS:
-            paths.append(default_path + path)
-
-        # Profile Folders
-        profiles = get_browser_profiles(userData_path)
-        paths.extend(profiles[0])
-        
-        print(Fore.LIGHTGREEN_EX + f"///// -> Brave Browser Detected - [{profiles[1]}] Profiles Detected")
-    
-    # Google Chrome
-    # ---------------------------------------------------------------------
-
-    if os.path.isdir(CHROME_PATH):
-        userData_path = CHROME_PATH + "\\User Data"
-        default_path = userData_path + "\\Default"
-
-        # Basic Folder
-        for path in BROWSERS_PATHS:
-            paths.append(default_path + path)
-        
-        # Profile Folders
-        profiles = get_browser_profiles(userData_path)
-        paths.extend(profiles[0])
-
-        print(Fore.LIGHTGREEN_EX + f"///// -> Google Chrome Browser Detected - [{profiles[1]}] Profiles Detected")
-
-    # Discord
-    # ---------------------------------------------------------------------
-
-    if os.path.isdir(DISCORD_PATH):
-
-        discord_delete_folders = [
-            "\\Cache\\Cache_Data",
-            "\\Code Cache",
-            "\\GPU_Cache"
-        ]
-
-        for path in discord_delete_folders:
-            paths.append(DISCORD_PATH + path)
-        
-        print(Fore.LIGHTGREEN_EX + "///// -> Discord Detected")
-    
-    # Spotify
-    # ---------------------------------------------------------------------
-    if os.path.isdir(SPOTIFY_PATH):
-
-        default_path = SPOTIFY_PATH + "\\Default"
-
-        # Basic Folders
-        for path in BROWSERS_PATHS:
-            if os.path.isdir(SPOTIFY_PATH + path): paths.append(SPOTIFY_PATH + path)
-            if os.path.isdir(default_path + path): paths.append(default_path + path)
-        
-        # Profile Folders
-        profiles = get_browser_profiles(SPOTIFY_PATH)
-        paths.extend(profiles[0])
-
-        print(Fore.LIGHTGREEN_EX + f"///// -> Spotify Detected - [{profiles[1]}] Profiles Detected")
-
-    wait_before_continue(5)
-    return paths
-
-
 # =================================================================================================================
 #                                                   CLEANER
 # =================================================================================================================
 
 def cleaner(path, days_threshold=None, size_threshold_mb=None):
-    """Elimina archivos y carpetas basándose en antigüedad o tamaño."""
+    """Deletes files and folders based on age or size."""
     global numVars
     global VERBOSE_MODE
     try:
@@ -200,7 +64,7 @@ def cleaner(path, days_threshold=None, size_threshold_mb=None):
             file_path = os.path.join(path, filename)
 
             try:
-                # Limpiar archivos
+                # Delete Files
                 if os.path.isfile(file_path) or os.path.islink(file_path):
                     if days_threshold and not is_file_old(file_path, days_threshold):
                         continue  # Saltar archivos recientes
@@ -214,7 +78,7 @@ def cleaner(path, days_threshold=None, size_threshold_mb=None):
                     numVars["Del_Files"] += 1
                     numVars["MB"] += size
 
-                # Limpiar carpetas
+                # Delete Folders
                 elif os.path.isdir(file_path):
                     size = get_file_size(file_path)
                     shutil.rmtree(file_path)
@@ -239,7 +103,6 @@ def cleaner(path, days_threshold=None, size_threshold_mb=None):
 # =================================================================================================================
 
 def main():
-    """Limpia carpetas temporales comunes con configuraciones avanzadas."""
     
     global numVars
 
